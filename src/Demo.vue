fa77f1ef8de3f81d474444b2bad1271190b88bcd
McCall <1059511459@qq.com>
2020-06-28 22:06:40 +0800

    study to destroyed

---
+++ src/Demo.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="red">
+        {{n}}
+        <button @click="add">+1</button>
+        Demo
+        <hr>
+    </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return {
+        n:0,
+      }
+    },
+    created() {
+      console.log('created 出现在内存中,前面加debugger验证，' +
+        '页面内容还没出现，这行内容就已经在控制台了')
+    },
+    mounted() {
+      console.log('mounted 出现在页面中')
+    },
+    updated() {
+      console.log('页面更新了，数字变了')
+    },
+    destroyed() {
+      //点击toggle按钮的时候，触发toggle方法，组件消亡
+      console.log('组件已经消亡了')
+    },
+    methods: {
+      add() {
+        this.n += 1
+      },
+    }
+  }
+</script>
+<style scoped>
+    .red {
+        color: darkred;
+    }
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
-import Vue from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
+const Vue = window.Vue
+// Vue.config.productionTip = false
 
-Vue.config.productionTip = false
+import Demo from './Demo.vue'
 
+Vue.component('Demo2', {})
 new Vue({
-  render: h => h(App),
+  components: {Demo},
+  data: {
+    visible: true
+  },
+  template: `
+      <div>
+<!--          调用toggle-->
+          <button @click="toggle">toggle</button>
+          <Demo v-if="visible === true"/>
+      </div>
+  `,
+  methods: {
+    toggle() {
+      this.visible = !this.visible
+      //toggle方法会将visible的数值取反。
+    }
+  }
 }).$mount('#app')
